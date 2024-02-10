b7acb153177d96f5fd9291e77d791c50f887b91c
beyond88 <muhin.cse.diu@gmail.com>
2024-02-10 22:39:58 +0600

    initial plugin files upload

---
+++ export-flick.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: ExportFlick
+ * Description: Export post and page easily.  
+ * Plugin URI: https://github.com/beyond88/give-kindness
+ * Author: Mohiuddin Abdul Kader
+ * Author URI: https://github.com/beyond88
+ * Version: 1.0.0
+ * Text Domain:       export-flick
+ * Domain Path:       /languages
+ * Requires PHP:      7.2
+ * Requires at least: 4.4
+ * Tested up to:      6.4.3
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== ExportFlick - Post and page export solution ===
+Contributors: hossain88
+Donate Link: https://github.com/beyond88
+Tags: export, page, post, import, wordpress
+Requires at least: 4.4
+Tested up to: 6.4.3
+Requires PHP: 7.2
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Export page and post easily.
+
+== Description ==
+
+👉 Official Demo Link: [Official Demo](https://github.com/beyond88/export-flick)
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial version released
